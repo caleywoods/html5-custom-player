0981d88b12a237c9bc96779f7bda23cadbe0660b
Caley Woods <caley.w@gmail.com>
2017-08-31 10:52:48 -0500

    added .then to play() call since it returns a promise

--- scripts.js
+++ scripts.js
@@ -4,18 +4,14 @@ const playBtn  = document.querySelector('.player__button');
 
 
 playBtn.onclick = () => {
-    //Play and Pause return promises, can we do something neat with that?
-    /* documentation seems to say that promises natively support then,
-     * I suppose it's fair to say that we only want to remove the playing
-     * class if the pause was successful?
-     */
+    // only play returns a promise, pause does not.
     if ( vidya.classList.contains('playing') ) {
         vidya.pause();
         vidya.classList.remove( 'playing' );
     } else {
-        vidya.play();
-        vidya.classList.add( 'playing' );
+        vidya.play().then(() => {
+            // Anything in here is called after the promise is fulfilled
+            vidya.classList.add( 'playing' );
+        });
     }
-
-    console.log( vidya.classList );
 }
