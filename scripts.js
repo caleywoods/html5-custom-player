025a7bd9f050527622c95999c06f31177fd70645
Caley Woods <caley.w@gmail.com>
2017-08-31 11:11:07 -0500

    Committing some comments in JS file. I think these are important to show throught process

--- scripts.js
+++ scripts.js
@@ -19,3 +19,22 @@ playBtn.onclick = () => {
             });
     }
 }
+
+/* <video> element is and HTMLVideoElement which inherits from HTMLMediaElement
+ * HTMLMediaElement contains a currentTime property which indicates the current playback time in seconds
+ * can probably use this for seeking by calling .currentTime() += <value of data-skip seconds>
+ *
+ * May want to do a check for duration to make sure adding 25 seconds doesn't exceed it, not sure how that is
+ * handled. May not be needed.
+ *
+ * method exists called fastSeek() that takes you to a given time
+ */
+
+// implement skip ahead -- seekable / seeking (bool)
+/* seekable returns a TimeRange object that contains the time ranges a user is able to seek to */
+
+// implement skip back
+
+// implement speed up (likely using vidya.playbackRate)
+
+// implement volume? (.volume a double from 0.0 muted to 1.0 loudest)
